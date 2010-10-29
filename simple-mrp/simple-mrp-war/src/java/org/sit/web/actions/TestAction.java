ed73aeab1b89f34afd27f59da4b2e59b3d8ea7e8
wisaruthk <wisaruthk@da538956-7233-81ce-13b9-475b612601be>
2010-10-29 14:58:49 +0000

    initial starter kit

---
+++ simple-mrp/simple-mrp-war/src/java/org/sit/web/actions/TestAction.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sit.web.actions;
+
+import org.sit.jpa.entities.TestUnit;
+
+/**
+ *
+ * @author wisaruthkea
+ */
+public class TestAction {
+    private TestUnit t;
+}
---
+++ simple-mrp/simple-mrp-war/src/java/org/sit/web/beans/BItem.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sit.web.beans;
+
+/**
+ *
+ * @author wisaruthkea
+ */
+public class BItem {
+
+}
