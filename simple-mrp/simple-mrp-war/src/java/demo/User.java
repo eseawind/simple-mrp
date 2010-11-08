d6e719dea43309cfa3d51eedcae50a07d173c2c7
wisaruthk@gmail.com <wisaruthk@gmail.com@da538956-7233-81ce-13b9-475b612601be>
2010-11-08 13:33:58 +0000

    Change from struts1 To Jsf1.2,richfaces

---
+++ simple-mrp/simple-mrp-war/src/java/demo/User.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package demo;
+
+/**
+ *
+ * @author kajook
+ */
+public class User {
+    private String name="";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
