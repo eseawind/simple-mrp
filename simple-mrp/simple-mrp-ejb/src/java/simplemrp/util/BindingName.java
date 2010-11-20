b93e3f9057471ed4d20522e041622086b0bff6ed
mr.sopon.s <mr.sopon.s@da538956-7233-81ce-13b9-475b612601be>
2010-11-20 15:50:58 +0000

    Add BindingName.java

---
+++ simple-mrp/simple-mrp-ejb/src/java/simplemrp/util/BindingName.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simplemrp.util;
+
+/**
+ *
+ * @author Golf
+ */
+public class BindingName {
+    public static final String CoFacadeRemote = "simplemrp.CoFacadeRemote";
+}
