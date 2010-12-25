79b121cde1e1199aa6062320400e48c9f0e112cc
wisaruthk <wisaruthk@da538956-7233-81ce-13b9-475b612601be>
2010-12-25 07:00:59 +0000

    batch file to extract ejb interface and web ref.

---
+++ simple-mrp/complie-ejb-interface.bat
@@ -0,0 +1,3 @@
+cd ".\simple-mrp-ejb\build\classes"
+jar cvf "..\..\..\simple-mrp-war\lib\simple-ejb-itnf.jar" simplemrp/facade/*.class simplemrp/entity/*.class simplemrp/util/*.class
+cd "../../../"
