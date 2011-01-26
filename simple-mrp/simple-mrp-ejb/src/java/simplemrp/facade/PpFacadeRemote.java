271535853e2f25764b242869d4cde7d7fb06dbd2
wisaruthk <wisaruthk@da538956-7233-81ce-13b9-475b612601be>
2011-01-26 15:35:50 +0000

    implement data

--- simple-mrp/simple-mrp-ejb/src/java/simplemrp/facade/PpFacadeRemote.java
+++ simple-mrp/simple-mrp-ejb/src/java/simplemrp/facade/PpFacadeRemote.java
@@ -5,7 +5,11 @@
 
 package simplemrp.facade;
 
+import java.util.Date;
+import java.util.List;
 import javax.ejb.Remote;
+import simplemrp.entity.Job;
+import simplemrp.entity.Job_stat;
 
 /**
  *
@@ -13,5 +17,12 @@ import javax.ejb.Remote;
  */
 @Remote
 public interface PpFacadeRemote {
-    
+    ////////////////// JOB ORDER ////////////////////
+    public List<Job_stat> listJobStat();
+    public Job getJob(String inputJobId);
+    public List<Job> searchJob(String searchJobKey);
+    public void removeJob(String jobid);
+    public String createJob(Job p_job);
+    public void editJob(Job p_job);
+    public List<Job> findByJobDate(Date p_jobDate);
 }
