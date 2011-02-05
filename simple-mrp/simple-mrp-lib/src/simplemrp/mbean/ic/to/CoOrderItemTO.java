52f7dcedf59fc1f7e1a5e9f20b4bcd03125612d9
wisaruthk <wisaruthk@da538956-7233-81ce-13b9-475b612601be>
2011-02-05 16:28:02 +0000

    implement CO shipped function search

---
+++ simple-mrp/simple-mrp-lib/src/simplemrp/mbean/ic/to/CoOrderItemTO.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simplemrp.mbean.ic.to;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author wisaruthkea
+ */
+public class CoOrderItemTO implements Serializable {
+    private String itemId;
+    private String wareHouseId;
+    private String locationId;
+    private Double toBeShip;
+
+    /**
+     * @return the itemId
+     */
+    public String getItemId() {
+        return itemId;
+    }
+
+    /**
+     * @param itemId the itemId to set
+     */
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    /**
+     * @return the wareHouseId
+     */
+    public String getWareHouseId() {
+        return wareHouseId;
+    }
+
+    /**
+     * @param wareHouseId the wareHouseId to set
+     */
+    public void setWareHouseId(String wareHouseId) {
+        this.wareHouseId = wareHouseId;
+    }
+
+    /**
+     * @return the locationId
+     */
+    public String getLocationId() {
+        return locationId;
+    }
+
+    /**
+     * @param locationId the locationId to set
+     */
+    public void setLocationId(String locationId) {
+        this.locationId = locationId;
+    }
+
+    /**
+     * @return the toBeShip
+     */
+    public Double getToBeShip() {
+        return toBeShip;
+    }
+
+    /**
+     * @param toBeShip the toBeShip to set
+     */
+    public void setToBeShip(Double toBeShip) {
+        this.toBeShip = toBeShip;
+    }
+}
